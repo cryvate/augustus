79a149f532bcc7d57ae23a5ae9ade6905b2c0f9a
0123user0123 <dixon.savannah@yahoo.co.uk>
2026-09-24 08:08:12 +0100

    Fix load file dialog prefill filter bug

--- src/window/file_dialog.c
+++ src/window/file_dialog.c
@@ -240,31 +240,20 @@ static void init(file_type type, file_dialog_type dialog_type)
     }
     data.dialog_type = dialog_type;
 
-    if (type == FILE_TYPE_SAVED_GAME && dialog_type == FILE_DIALOG_SAVE) {
-        snprintf(data.selected_file, FILE_NAME_MAX, "%s", game_file_get_original_save_name());
+    if (dialog_type == FILE_DIALOG_SAVE) {
+        if (type == FILE_TYPE_SAVED_GAME) {
+            snprintf(data.selected_file, FILE_NAME_MAX, "%s", game_file_get_original_save_name());
+        } else if (strlen(data.file_data->last_loaded_file) > 0) {
+            snprintf(data.selected_file, FILE_NAME_MAX, "%s", data.file_data->last_loaded_file);
+        } else {
+            string_copy(lang_get_string(9, type == FILE_TYPE_SCENARIO ? 7 : 6), data.typed_name, FILE_NAME_MAX);
+            encoding_to_utf8(data.typed_name, data.selected_file, FILE_NAME_MAX, encoding_system_uses_decomposed());
+        }
+        file_remove_extension(data.selected_file);
         encoding_from_utf8(data.selected_file, data.typed_name, FILE_NAME_MAX);
-        file_append_extension(data.selected_file, data.file_data->extension, FILE_NAME_MAX);
-    } else if (strlen(data.file_data->last_loaded_file) > 0) {
-        if (strncmp(data.file_data->last_loaded_file, "autosave-resume-", 16) == 0) {
-            memmove(data.file_data->last_loaded_file,
-                    data.file_data->last_loaded_file + 16,
-                    strlen(data.file_data->last_loaded_file + 16) + 1);
-        }
-        snprintf(data.selected_file, FILE_NAME_MAX, "%s", data.file_data->last_loaded_file);
-        if (data.dialog_type == FILE_DIALOG_SAVE) {
-            file_remove_extension(data.selected_file);
-        }
-        encoding_from_utf8(data.selected_file, data.typed_name, FILE_NAME_MAX);
-        if (data.dialog_type == FILE_DIALOG_SAVE) {
-            file_append_extension(data.selected_file, data.file_data->extension, FILE_NAME_MAX);
-        }
-    } else if (dialog_type == FILE_DIALOG_SAVE) {
-        // Suggest default filename
-        string_copy(lang_get_string(9, type == FILE_TYPE_SCENARIO ? 7 : 6), data.typed_name, FILE_NAME_MAX);
-        encoding_to_utf8(data.typed_name, data.selected_file, FILE_NAME_MAX, encoding_system_uses_decomposed());
         file_append_extension(data.selected_file, data.file_data->extension, FILE_NAME_MAX);
     } else {
-        // Use empty string
+        // Use empty string for load dialogs so file list is not filtered
         data.typed_name[0] = 0;
         data.selected_file[0] = 0;
     }
